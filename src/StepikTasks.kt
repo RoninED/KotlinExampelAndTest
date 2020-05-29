ebaf193a49040a60d753b55b8b6b41ef82b80716
Eduard Gimadiev <nameeduard@yandex.ru>
2020-05-29 10:53:33 +0300

    add jsonTest files

--- src/StepikTasks.kt
+++ src/StepikTasks.kt
@@ -1,23 +1,14 @@
-class StepikTasks {
-}
-
-class EngineNotFoundException() : Exception()
-class SensorsMeltException() : Exception()
-class SphinxesException() : Exception()
-
 fun main(args: Array<String>) {
 
-    fun calcNullValues(input: Array<Int?>):Array<Int>{
-        var a:Int = 0
-        var b:Int = 0
-        for (tmp in input)
-            when {
-                tmp ==null -> a++
-                else -> b+=tmp
-            }
 
-return arrayOf(a,b)
+
+    try {
+        throw Exception()
+
+
+
+    } catch (e: Exception) {
+        println("aaa")
     }
-    calcNullValues(arrayOf(null, 1,2)).forEach { print("$it ") }
 
 }
